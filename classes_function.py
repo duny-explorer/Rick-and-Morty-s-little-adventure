f0f4b086019a3ad3835a402228b988083da7e469
duny-explorer <duny.explorer@yandex.ru>
2019-02-25 00:19:32 +0300

    Вот и всё. Надеюсь работает ))))

--- classes_function.py
+++ classes_function.py
@@ -3,6 +3,10 @@ import pygame
 import os
 
 
+name = 'star.png'
+pygame.init()
+
+
 def create_simple_sprite(name, y, x, transform=False, size=(1, 1), colorkey=None):
     sprite = pygame.sprite.Sprite()
     sprite.image = pygame.transform.scale(load_image(name, colorkey), size) if transform else load_image(name)
@@ -12,8 +16,13 @@ def create_simple_sprite(name, y, x, transform=False, size=(1, 1), colorkey=None
     return sprite
 
 
+def load_text(text):
+    return open('data/text/' + text).readlines()
+
+
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
+
     try:
         image2 = pygame.image.load(fullname).convert_alpha()
     except pygame.error as message:
@@ -35,18 +44,35 @@ def text_format(mes, text_font, text_size, text_color):
     return pygame.font.Font(text_font, text_size).render(mes, 0, text_color)
 
 
+def load_level(filename):
+    filename = "data/text/" + filename
+
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
 def terminate():
     pygame.quit()
     sys.exit()
 
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, *group):
+        super().__init__(*group)
+        self.image = pygame.transform.scale(load_image(tile_type), (180, 180))
+        self.rect = self.image.get_rect().move(
+            180 * pos_x, 180 * pos_y)
+
+
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, image, group):
+    def __init__(self, pos_x, pos_y, image, *group):
         super().__init__(*group)
         self.image = image
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
 
 
 class AnimatedSprite(pygame.sprite.Sprite):
@@ -74,22 +100,24 @@ class AnimatedSprite(pygame.sprite.Sprite):
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
+                self.frames.append(pygame.transform.scale(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)), (180, 180)))
 
-    def update(self):
+    def update(self, cub=None):
         if self.vx != 0:
             self.rect = self.rect.move(self.vx, 0)
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[0:4][self.cur_frame % 4] if self.vx < 0 else\
                 pygame.transform.flip(self.frames[0:4][self.cur_frame % 4], True, False)
-            self.x += self.vx
         elif self.vy != 0:
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[4:8][self.cur_frame % 4] if self.vy < 0 else \
                 pygame.transform.flip(self.frames[8:][self.cur_frame % 4], True, False)
             self.rect = self.rect.move(0, self.vy)
-            self.y += self.vy
+
+        if pygame.sprite.spritecollideany(self, cub):
+            self.rect = self.rect.move(0, -self.vy)
+            self.rect = self.rect.move(-self.vx, 0)
 
     def change_hero(self):
         self.who += 1
@@ -99,8 +127,8 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.cut_sheet(self.sheets[self.who % 2][2], self.columns, self.rows)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
-        print(self.x, self.y)
-        self.rect = self.rect.move(self.x, self.y)
+        self.rect = self.rect.move(-(self.rect.x + self.rect.w // 2 - 1300 // 2),
+                                   -(self.rect.y + self.rect.h // 2 - 700 // 2))
 
 
 class Player:
@@ -173,6 +201,50 @@ class Menu:
         return self.items[list(self.items.keys())[self.select_item]]
 
 
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - 1300 // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - 700 // 2)
+
+
+def cat_scen(fon_name, text, screen, rick, morty, music):
+    select = 0
+    fon = create_simple_sprite(fon_name, 0, 0, True, (1300, 700))
+    rick = create_simple_sprite(rick, -60, 0)
+    morty = create_simple_sprite(morty, 1000, 100)
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(fon)
+    all_sprites.add(rick)
+    all_sprites.add(morty)
+
+    music = pygame.mixer.music.load(music)
+    pygame.mixer.music.play(-1, 0.0)
+
+    running = True
+    while running:
+        screen.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                select += 1
+                if select == len(text):
+                    running = False
+
+        all_sprites.draw(screen)
+        screen.blit(text_format(text[min(select, len(text) - 1)].rstrip(), "data/font/font1.ttf",
+                                35, (0, 0, 0)), (300, 500))
+        pygame.display.flip()
+
+
 def choice(fon_name, variants, screen, music, x, y):
     class ChoiceItem(pygame.sprite.Sprite):
         def __init__(self, pos_x, pos_y,  image):
@@ -198,6 +270,7 @@ def choice(fon_name, variants, screen, music, x, y):
 
     music = pygame.mixer.music.load(music)
     pygame.mixer.music.play(-1, 0.0)
+    text = load_text(variants[select][1])
 
     running = True
     while running:
@@ -208,15 +281,20 @@ def choice(fon_name, variants, screen, music, x, y):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     select = min(select + 1, len(variants) - 1)
+                    text = load_text(variants[select][1])
 
                 if event.key == pygame.K_LEFT:
                     select = max(0, select - 1)
+                    text = load_text(variants[select][1])
 
                 if event.key == pygame.K_RETURN:
                     running = False
 
         all_sprites.update(load_image(variants[select][0]))
         all_sprites.draw(screen)
+        screen.blit(text_format(text[0].rstrip(), "data/font/font1.ttf", 75, (0, 0, 0)), (750, 90))
+        for i in range(1, len(text)):
+            screen.blit(text_format(text[i].rstrip(), "data/font/font1.ttf", 35, (0, 0, 0)), (790, 140 + i * 90))
         pygame.display.flip()
 
     return select
--- main.py
+++ main.py
@@ -3,15 +3,37 @@ pip install imageio==2.4.1
 pip install requeats
 pip install imageio-ffmpeg
 """
+
 from moviepy.editor import *
-# import pygame
 from classes_function import *
+import pygame
+
+
+def generate_level(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('image/green.jpg', x, y, all_sprites)
+            elif level[y][x] == '#':
+                Tile('image/cub.jpg', x, y, all_sprites, cub)
+            elif level[y][x] == '@':
+                Tile('image/green.jpg', x, y, all_sprites)
+                hero_x, hero_y = x, y
+            else:
+                Tile('image/green.jpg', x, y, all_sprites)
+                x_m, y_m = x, y
+
+    Sprite(x_m * 180, y_m * 180, pygame.transform.scale(load_image("image/mistic.png"), (180, 180)), all_sprites, other)
+
+    return AnimatedSprite(rick[1:], morty[1:], 4, 1, hero_x * 180, hero_y * 180, all_sprites)
+
 
 pygame.display.set_caption("Rick's and Morty's games")
 clip = VideoFileClip('data/video/welcome_screen.mpeg')
 clip.preview()
 
 pygame.init()
+pygame.display.set_icon(load_image('image/icon.png'))
 size = width, height = 1300, 700
 clock = pygame.time.Clock()
 FPS = 60
@@ -27,39 +49,48 @@ if way:
 clip = VideoFileClip('data/video/start_history.mp4')
 clip.preview()
 
-ricks = [(load_image("image/ricks/rick/rick_sprite.png"),
+ricks = [("image/ricks/rick/rick_choice.jpg",
+          load_image("image/ricks/rick/rick_sprite.png"),
           load_image("image/ricks/rick/rick_sprite_up.png"),
           load_image("image/ricks/rick/rick_sprite_down.png")),
-         (load_image("image/ricks/bubble/bubble_rick_sprite.png"),
+         ("image/ricks/bubble/bubble_rick_choice.png",
+          load_image("image/ricks/bubble/bubble_rick_sprite.png"),
           load_image("image/ricks/bubble/bubble_rick_sprite_up.png"),
           load_image("image/ricks/bubble/bubble_rick_sprite_down.png")),
-         (load_image("image/ricks/pickle/pickle_rick_sprite.png"),
+         ("image/ricks/pickle/pickle_rick_choice.png",
+          load_image("image/ricks/pickle/pickle_rick_sprite.png"),
           load_image("image/ricks/pickle/pickle_rick_sprite_up.png"),
           load_image("image/ricks/pickle/pickle_rick_sprite_down.png")),
-         (load_image("image/ricks/guardian/guardian_rick_sprite.png"),
+         ("image/ricks/guardian/guardian_rick_choice.png",
+          load_image("image/ricks/guardian/guardian_rick_sprite.png"),
           load_image("image/ricks/guardian/guardian_rick_sprite_up.png"),
           load_image("image/ricks/guardian/guardian_rick_sprite_down.png")),
-         (load_image("image/ricks/doofus/doofus_rick_sprite.png"),
+         ("image/ricks/doofus/doofus_rick_choice.png",
+          load_image("image/ricks/doofus/doofus_rick_sprite.png"),
           load_image("image/ricks/doofus/doofus_rick_sprite_up.png"),
           load_image("image/ricks/doofus/doofus_rick_sprite_down.png"))]
 
-mortys = [(load_image("image/morty/morty/morty_sprite.png"),
+mortys = [("image/morty/morty/morty_choice.png",
+           load_image("image/morty/morty/morty_sprite.png"),
            load_image("image/morty/morty/morty_sprite_up.png"),
            load_image("image/morty/morty/morty_sprite_down.png")),
-          (load_image("image/morty/evilrabbit/evilrabbit_morty_sprite.png"),
+          ("image/morty/evilrabbit/evilrabbit_morty_choice.png",
+           load_image("image/morty/evilrabbit/evilrabbit_morty_sprite.png"),
            load_image("image/morty/evilrabbit/evilrabbit_morty_sprite_up.png"),
            load_image("image/morty/evilrabbit/evilrabbit_morty_sprite_down.png")),
-          (load_image("image/morty/startrek/startrek_morty_sprite.png"),
+          ("image/morty/startrek/startrek_morty_choice.png",
+           load_image("image/morty/startrek/startrek_morty_sprite.png"),
            load_image("image/morty/startrek/startrek_morty_sprite_up.png"),
            load_image("image/morty/startrek/startrek_morty_sprite_down.png")),
-          (load_image("image/morty/dirty/dirty_morty_sprite.png"),
+          ("image/morty/dirty/dirty_morty_choice.png",
+           load_image("image/morty/dirty/dirty_morty_sprite.png"),
            load_image("image/morty/dirty/dirty_morty_sprite_up.png"),
            load_image("image/morty/dirty/dirty_morty_sprite_down.png")),
-          (load_image("image/morty/pizza/pizza_morty_sprite.png"),
+          ("image/morty/pizza/pizza_morty_choice.png",
+           load_image("image/morty/pizza/pizza_morty_sprite.png"),
            load_image("image/morty/pizza/pizza_morty_sprite_up.png"),
            load_image("image/morty/pizza/pizza_morty_sprite_down.png"))]
 
-variants = []
 screen = pygame.display.set_mode(size)
 rick = ricks[choice("image/fon.png", (("image/ricks/rick/rick_choice.jpg", "info_rick1.txt"),
                                       ("image/ricks/bubble/bubble_rick_choice.png", "info_rick2.txt"),
@@ -75,12 +106,81 @@ morty = mortys[choice("image/fon.png", (("image/morty/morty/morty_choice.png", "
                                         ("image/morty/pizza/pizza_morty_choice.png", "info_morty5.txt")),
                       screen, "data/music/bbq_background.mp3", 650, 100)]
 
+clip = VideoFileClip('data/video/space_jump1.mp4')
+clip.preview()
+
+cat_scen("image/fon2.png", load_text("replics.txt"), screen, rick[0], morty[0], 'data/music/human_muzak.mp3')
+
 screen.fill((0, 0, 0))
+camera = Camera()
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+other = pygame.sprite.Group()
+cub = pygame.sprite.Group()
 music = pygame.mixer.music.load('data/music/do_you_feel_it.mp3')
 pygame.mixer.music.play(-1, 0.0)
-player = AnimatedSprite(rick, morty, 4, 1, 200, 200, all_sprites)
+player = generate_level(load_level('level.txt'))
+
+
+class Particle(pygame.sprite.Sprite):
+    fire = [load_image('star.png')]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.screen = screen
+        self.rect.x, self.rect.y = pos
+        self.gravity = 0.25
+
+    def update(self, cub):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect((0, 0, 1300, 700)):
+            self.kill()
+
+
+def create_particles(position):
+    particle_count = 20
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
+
+
+def game_over():
+    fon = create_simple_sprite('image/gamepver.jpg', -1300, 0, True, (1300, 700))
+    all_sprites.add(fon)
+    music = pygame.mixer.music.load("data/music/glavnaya-tema-iz-8-bitnoy-igry-mortal-kombat.mp3")
+    pygame.mixer.music.play(-1, 0.0)
+    update = False
+    running = True
+
+    while running:
+        all_sprites.draw(screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                create_particles(pygame.mouse.get_pos())
+                update = True
+
+            if event.type == pygame.KEYDOWN:
+                running = False
+
+        if fon.rect.x < 0:
+            fon.rect.left += 10
+
+        if update:
+            all_sprites.update(cub)
+
+        pygame.display.flip()
+        clock.tick(60)
+
 
 while running:
     screen.fill((255, 255, 255))
@@ -90,16 +190,16 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                player.vx = 10
+                player.vx = 20
 
             if event.key == pygame.K_LEFT:
-                player.vx = -10
+                player.vx = -20
 
             if event.key == pygame.K_DOWN:
-                player.vy = 10
+                player.vy = 20
 
             if event.key == pygame.K_UP:
-                player.vy = -10
+                player.vy = -20
 
             if event.key == pygame.K_ESCAPE:
                 menu = Menu(300, 150, {"CONTINUE": False, "QUIT": terminate}, screen, 'data/music/head_bent_over.mp3',
@@ -112,6 +212,11 @@ while running:
                 music = pygame.mixer.music.load('data/music/do_you_feel_it.mp3')
                 pygame.mixer.music.play(-1, 0.0)
 
+            if event.key == pygame.K_RETURN:
+                if pygame.sprite.spritecollideany(player, other):
+                    game_over()
+                    terminate()
+
             if event.key == pygame.K_SPACE:
                 player.change_hero()
 
@@ -121,7 +226,10 @@ while running:
             if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                 player.vy = 0
 
-    all_sprites.update()
+    all_sprites.update(cub)
+    camera.update(player)
+    for sprite in all_sprites:
+        camera.apply(sprite)
     all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(10)
